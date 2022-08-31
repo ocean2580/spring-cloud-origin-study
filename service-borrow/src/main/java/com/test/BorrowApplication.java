93afc16bf29baa2329bc3b6bd187b20caf92eec0
Ocean <2080917477@qq.com>
2022-08-31 12:40:15 +0800

    hystrix 实现服务降级

--- service-borrow/src/main/java/com/test/BorrowApplication.java
+++ service-borrow/src/main/java/com/test/BorrowApplication.java
@@ -3,10 +3,12 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients // feign
+@EnableHystrix  // hystrix
 public class BorrowApplication {
     public static void main(String[] args) {
         SpringApplication.run(BorrowApplication.class, args);
--- service-borrow/src/main/java/com/test/controller/BorrowController.java
+++ service-borrow/src/main/java/com/test/controller/BorrowController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.test.entity.UserBorrowDetail;
 import com.test.service.BorrowService;
 import com.test.service.impl.BorrowServiceImpl;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/borrow")
@@ -18,7 +20,15 @@ public class BorrowController {
     BorrowService service;
 
     @GetMapping("/{uid}")
+    @HystrixCommand(fallbackMethod = "onError")    //使用@HystrixCommand来指定备选方案
     UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid){
         return service.getUserBorrowDetailByUid(uid);
     }
+
+    //备选方案，这里直接返回空列表了
+    //注意参数和返回值要和上面的一致
+    UserBorrowDetail onError(int uid){
+        return new UserBorrowDetail(null, Collections.emptyList());
+    }
+
 }
